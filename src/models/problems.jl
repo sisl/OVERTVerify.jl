ea3905ad446bea443849f23715fbddccfb4cd1cf
chelseas <csidrane@stanford.edu>
2022-02-16 16:53:30 -0800

    editing get_overt_dynamics

--- src/models/problems.jl
+++ src/models/problems.jl
@@ -66,13 +66,19 @@ end
 function get_overt_dynamics(exprs, vars_to_time=[], ϵ=1e-4)
 	# return this function for a given problem.
 	function overt_dynamics_fun(range_dict::Dict{Symbol, Array{T, 1}} where {T <: Real}, N_OVERT::Int=-1, t_idx::Union{Int, Nothing}=nothing)
+		@debug "exprs before timing are: $(exprs)"
 		# first determine whether dynamics need to be timestamped 
+		# TODO: re-writing the exprs variable is kept every time this (closure?) is called. so can't do that. 
+		# TODO: AND dictionary keys are not re-mapped. e.g. mapping θ to θ_1 (but I think that is already done inside setup_overt_and_controller_constraints)
 		if !isnothing(t_idx)
-			exprs = time_exprs(exprs, vars_to_time, t_idx)
-			@debug("timed expressions: $(exprs)")
+			exprs_to_approx = time_exprs(exprs, vars_to_time, t_idx)
+			@debug("timed expressions: $(exprs_to_approx)")
+		else
+			exprs_to_approx = exprs
 		end
 		oAs = OVERT.OverApproximation[]
-		for expr in exprs 
+		for expr in exprs_to_approx 
+			@debug "range dict is: $(range_dict)"
 			push!(oAs, overapprox(expr, range_dict; N=N_OVERT, ϵ=ϵ))
 		end
 		oA_out = add_overapproximate(oAs)
